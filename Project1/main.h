764fd7654b50a91f7c6223bd438825dfed351711
Noia <49865944+noiia@users.noreply.github.com>
2023-11-10 13:15:41 +0100

    "First commit"

---
+++ Project1/main.h
@@ -0,0 +1,13 @@
+﻿#include "BDD.h"
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int __clrcall WinMain(array<String^>^ args)
+{
+    BDD^ mabdd = gcnew BDD();
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Project1::MyForm monFormulaire(mabdd);
+    Application::Run(% monFormulaire);
+}
